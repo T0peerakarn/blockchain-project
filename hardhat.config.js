5d7fbef4af824561bdab3ff2aa628241be580bae
bp40 <60870525+bp40@users.noreply.github.com>
2024-12-12 01:05:13 +0700

    perf: enable optimizer for solidity/hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,4 +4,11 @@ require("solidity-coverage");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 200,
+    },
+    viaIR: true,
+  },
 };
