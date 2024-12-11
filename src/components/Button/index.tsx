875374999331afddb35b030655fc1129ee3f6cf3
T0peerakarn <t0p.eerakarn@gmail.com>
2024-12-11 22:11:06 +0700

    feat: fix button color

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -6,7 +6,7 @@ const Button = ({ title, ...buttonProps }: ButtonProps) => {
   return (
     <button
       {...buttonProps}
-      className="bg-[#2F5DA8] hover:bg-[#93b5ed] rounded-full px-12 py-2 josefin-sans text-white text-xl font-medium shadow-lg transition-all duration-150"
+      className="bg-[#2F5DA8] hover:bg-[#709FEB] rounded-full px-12 py-2 josefin-sans text-white text-xl font-medium shadow-lg transition-all duration-150"
     >
       {title}
     </button>
