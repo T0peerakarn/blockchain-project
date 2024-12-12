bff398d176506dd792f586896babb3f2942ec974
Peerakarn Thongsata <60547443+T0peerakarn@users.noreply.github.com>
2024-12-12 17:21:52 +0700

    feat: Add DoctorLandingPage (#9)
    * feat: add style to TextInput
    
    * feat: add DoctorLandingPage

--- src/components/LeftMenuBar/index.tsx
+++ src/components/LeftMenuBar/index.tsx
@@ -1,21 +1,24 @@
+"use client";
+
 import React, { useState } from "react";
 import LeftMenuButton from "@/components/LeftMenuButton";
 
-const menuItems = [
-  { title: "Appointments", onClick: () => console.log("Appointments clicked") },
-  { title: "Medical History", onClick: () => console.log("Medical History clicked") },
-  { title: "Lab Results", onClick: () => console.log("Lab Results clicked") },
-  { title: "Reports", onClick: () => console.log("Reports clicked") },
-  { title: "Settings", onClick: () => console.log("Settings clicked") },
-];
+interface LeftMenuBarProps {
+  menuItems: { title: string; onClick: () => void }[];
+  defaultItem?: string;
+}
 
-const LeftMenuBar: React.FC = () => {
-  const [selectedItem, setSelectedItem] = useState<string>("Appointments");
-  const name = "Bhutas"
+const LeftMenuBar = ({ menuItems, defaultItem }: LeftMenuBarProps) => {
+  const [selectedItem, setSelectedItem] = useState<string>(
+    defaultItem ?? menuItems[0].title
+  );
+  const name = "Bhutas";
 
   return (
     <div className="w-[20%] bg-[#709FEB] p-4">
-        <h1 className="ml-6 mt-6 mb-10 josefin-sans text-xl text-white">Hello, {name}!</h1>
+      <h1 className="ml-6 mt-6 mb-10 josefin-sans text-xl text-white">
+        Hello, {name}!
+      </h1>
       <ul className="flex flex-col gap-4 text-white">
         {menuItems.map((item, index) => (
           <li key={index}>
